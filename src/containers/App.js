97cb3f51ea75e149bc148df35a3066f0123ad28c
Marcus Bischof <marcusbischof@marcuss-mbp.attlocal.net>
2019-02-27 22:45:25 -0600

    now to work on info window...

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,62 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactDOMServer from 'react-dom/server';
+
+// New library that I'm testing.
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
+import { compose } from "recompose";
 
 // Google maps wrapper that makes it easy to overlay components on top of google map.
 import GoogleMapReact from 'google-map-react';
+import Geocode from "react-geocode";
 
 // Styled components.
 import styled from 'styled-components';
 
 import { fetchLocations } from '../helpers/api';
 
-// Testing.
+const api_key = '';
 const MarcusTestingComponent = ({ text }) => <div>{text}</div>;
 
+const MyMapComponent = compose(withScriptjs, withGoogleMap)(props => {
+  // to continue .. https://github.com/fullstackreact/google-maps-react/blob/master/README.md
+  return (
+    <GoogleMap defaultZoom={4} defaultCenter={{ lat: 41.850033, lng: -87.650052 }}>
+        {props.markers.map(marker => {
+          return (
+            <Marker
+              key={marker.id}
+              position={{ lat: marker.lat, lng: marker.lng }}
+            >
+            </Marker>
+          )
+        })}
+    </GoogleMap>
+  )
+})
+
 class App extends React.Component {
-  componentDidMount() {
-    //fetchLocations()
-      //.then(response => console.log(response))
-      //.catch(error => console.log(error));
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      markers : [],
+      markers_loaded : false
+    };
   }
-  renderMarkers(map, maps) {
+
+  componentWillUpdate(nextProps, nextState) {
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  }
+
+  geocode_addresses() {
     fetchLocations()
       .then(response => {
 
-        // Instantiate a geocoder that can provide (lat, lng) tuples for the
-        // addresses in the locations array.
-        var geocoder = new google.maps.Geocoder();
+        // Set our API key.
+        Geocode.setApiKey("");
 
         ////////////////////////////////////////////////////////////////////////
         //
@@ -33,70 +65,44 @@ class App extends React.Component {
         // the address to our google maps object.
         //
         ////////////////////////////////////////////////////////////////////////
+        for ( let i = 0; i < response.length; i++ ) {
 
-        console.log(response)
-
-        for (let i = 0; i < response.length; i++) {
-          geocoder.geocode( { 'address': response[i].address }, function(results, status) {
-            // Checks to see whether we get a (lat, lng) tuple.
-            if (status == google.maps.GeocoderStatus.OK) {
-              let contentString = '<div id="content">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h1 id="firstHeading" class="firstHeading">' + response[i].slug + '</h1>'+
-                '<div id="bodyContent">'+
-                '<p> Address ' + response[i].address + '</p>' +
-                '<button value=\'' + i + '\' onclick={display_details_component()}>View Details.</button>' +
-                '</div>'+
-                '</div>';
-              // Taken from: https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
-              let infowindow = new google.maps.InfoWindow({
-                content: contentString
-              });
-              // Add a marker to our map using our geocoded address!
-              let marker = new maps.Marker({
-                position: {
-                  lat: results[0].geometry.location.lat(),
-                  lng: results[0].geometry.location.lng()
-                },
-                icon: {
-                  url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-                },
-                map
-              });
-              // Taken from: https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
-              marker.addListener('click', function() {
-                infowindow.open(map, marker);
-              });
+          // Get latidude & longitude from address.
+          Geocode.fromAddress(response[i].address).then(
+            response => {
+              const { lat, lng } = response.results[0].geometry.location;
+              this.setState(prevState => ({
+                markers : [...prevState.markers, { 'lat' : lat , 'lng' : lng}]
+              }))
+            },
+            error => {
+              console.error(error);
             }
+          );
 
-          });
         }
 
       })
       .catch(error => console.log(error));
   }
+
+  componentDidMount() {
+      this.geocode_addresses()
+  }
+
   render() {
     return (
       <span>
         <h1>Locations Map Exercise!</h1>
-        <div style={{ height: '100vh', width: '100%' }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: '' }}
-            defaultCenter={{
-              lat: 59.95,
-              lng: 30.33
-            }}
-            yesIWantToUseGoogleMapApiInternals={true}
-            onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
-            defaultZoom={11}>
-            <MarcusTestingComponent
-              lat={59.955413}
-              lng={30.337844}
-              text={'Kreyser Avrora'}
-            />
-          </GoogleMapReact>
-        </div>
+        <MyMapComponent
+          googleMapURL={"https://maps.googleapis.com/maps/api/js?key=" + api_key + "&v=3.exp&libraries=geometry,drawing,places"}
+          isMarkerShown
+          markers={this.state.markers}
+          markers_loaded={this.state.markers_loaded}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          />
       </span>
     );
   }
---
+++ src/containers/App_old.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOMServer from 'react-dom/server';
+
+// New library that I'm testing.
+import {GoogleMap, Marker, InfoWindow} from 'react-google-maps';
+
+// Google maps wrapper that makes it easy to overlay components on top of google map.
+import GoogleMapReact from 'google-map-react';
+
+// Styled components.
+import styled from 'styled-components';
+
+import { fetchLocations } from '../helpers/api';
+
+// Testing.
+const MarcusTestingComponent = ({ text }) => <div>{text}</div>;
+
+const InfoWindowButton = ({ text }) => {
+  <button>"yooo"</button>
+}
+
+
+class App extends React.Component {
+  state = {
+    markers : []
+  }
+  componentDidMount() {
+    //fetchLocations()
+      //.then(response => console.log(response))
+      //.catch(error => console.log(error));
+  }
+  renderMarkers(map, maps) {
+    fetchLocations()
+      .then(response => {
+
+        // Instantiate a geocoder that can provide (lat, lng) tuples for the
+        // addresses in the locations array.
+        var geocoder = new google.maps.Geocoder();
+
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // We loop through each of the eventual pins in the locations array,
+        // and we get the (lat, lng) tuple for this address in order to add
+        // the address to our google maps object.
+        //
+        ////////////////////////////////////////////////////////////////////////
+
+        console.log(response)
+
+        for (let i = 0; i < response.length; i++) {
+          geocoder.geocode( { 'address': response[i].address }, function(results, status) {
+            // Checks to see whether we get a (lat, lng) tuple.
+            if (status == google.maps.GeocoderStatus.OK) {
+              let testing = function() {
+                console.log('second dawg')
+              }
+              console.log(this)
+              //let cs = ReactDOMServer.renderToString(<InfoWindowButton text={"Kreyser Avrora"} />);
+              let contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">' + response[i].slug + '</h1>'+
+                '<div id="bodyContent">'+
+                '<p> Address ' + response[i].address + '</p>' +
+                '<button value=\'' + i + '\' onclick="testing()">View Details.</button>' +
+                '</div>'+
+                '</div>';
+              // Taken from: https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
+              let infowindow = new google.maps.InfoWindow({
+                content: contentString
+              });
+              console.log(infowindow)
+              // Add a marker to our map using our geocoded address!
+              let marker = new maps.Marker({
+                position: {
+                  lat: results[0].geometry.location.lat(),
+                  lng: results[0].geometry.location.lng()
+                },
+                icon: {
+                  url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                },
+                map
+              });
+              // Taken from: https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
+              marker.addListener('click', function() {
+                infowindow.open(map, marker);
+              });
+            }
+
+          });
+        }
+
+      })
+      .catch(error => console.log(error));
+  }
+  render() {
+    return (
+      <span>
+        <h1>Locations Map Exercise!</h1>
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyAWHOVtUO1w1lHaUYVOQvD23bhez4LQUag' }}
+            defaultCenter={{
+              lat: 41.850033,
+              lng: -87.6500523
+            }}
+            yesIWantToUseGoogleMapApiInternals={true}
+            onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
+            defaultZoom={4}>
+          </GoogleMapReact>
+        </div>
+      </span>
+    );
+  }
+}
+
+export default App;
